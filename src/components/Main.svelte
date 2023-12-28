1a366d08f5db382ebcb4b1f225fa7b017a478fb2
Arkadiusz Dawid <arkadiusz.dawid.99@gmail.com>
2023-12-28 12:45:33 +0100

    Add Header and and first half of Main content

---
+++ src/components/Main.svelte
@@ -0,0 +1,36 @@
+<script>
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+  <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+    <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+      <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+        Hi! I'm <span class="poppins text-violet-400">Arek</span> Dawid <br/>
+        Frontend <span class="poppins text-violet-400">Developer</span>
+      </h2>
+      <p class="text-base sm:text-lg md:text-xl">
+        My <span class="text-violet-400">favorite tech</span> includes JavaScript, Vue.js React.js, and TailwindCSS!
+      </p>
+      <a href="https://www.linkedin.com/in/adawid/" target="_blank" rel="noopener"  class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950">
+        <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"/>
+        <h4 class="relative z-9">Get in touch &rarr;</h4>
+      </a>
+    </div>
+    <div class="relative shadow-2xl grid place-items-center">
+      <img src={"images/profile.png"} alt="Profile" class="object-cover z-[2] w-[50vmin]"/>
+    </div>
+  </section>
+  
+  
+  <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+    <div class="flex flex-col gap-2 text-center">
+      <h6 class="text-lg sm:text-xl md:text-2xl">A few of my creative endeavors.</h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">Curious to <span class="poppins text-violet-400">see</span> my work?</h3>
+    </div>
+    <a href="https://github.com/areckt" target="_blank" rel="noopener" class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200">
+      <i class="fa-regular fa-circle-play"></i>
+      <p>Watch the video</p>
+    </a>
+  </section>
+</main>
