4e3b2e29f837463fd4d84e0db015aadc58625ed7
Arkadiusz Dawid <arkadiusz.dawid.99@gmail.com>
2023-12-30 21:28:22 +0100

    Add gh-pages package

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({strict: false}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-	}
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
